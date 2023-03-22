9bcb74fc2aa6bf69d6e2a8027bc77445db9fbfe6
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-03-22 19:49:48 -0400

    changed font size on nav bar

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borederRadius = '50px'
+}
+footer.style.borderStyle = 'solid'
 
 // Select some elements...
